908fdb22ce92d416a4cb0515c3b8f2ed2eeb7402
emmyari <39984190+emmyari@users.noreply.github.com>
2022-01-18 14:31:10 +0530

    solved max subarray Arijeet

--- arijeet/src/MaximumSubarray/Solution.java
+++ arijeet/src/MaximumSubarray/Solution.java
@@ -1,5 +1,24 @@
 package MaximumSubarray;
 
 public class Solution {
-    
+    //static int maxSum = Integer.MIN_VALUE; 
+    public static int maxSubArray(int[] nums) {
+        if(nums.length < 1){
+            System.out.println("cant accept null values\n error code :");
+            return Integer.MIN_VALUE;
+        }
+        int maxSum=nums[0];
+        int currentSum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            currentSum=Math.max(nums[i], nums[i]+currentSum);
+            maxSum=Math.max(currentSum,maxSum);
+        }
+        return maxSum;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{ -2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
 }
