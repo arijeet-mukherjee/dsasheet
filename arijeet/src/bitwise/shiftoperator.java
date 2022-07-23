b7e73bfe649fd9daa83ae83d06fcb944b42408b5
emmyari <39984190+emmyari@users.noreply.github.com>
2022-07-24 00:31:09 +0530

    modified shift

--- arijeet/src/bitwise/shiftoperator.java
+++ arijeet/src/bitwise/shiftoperator.java
@@ -19,9 +19,37 @@ public class shiftoperator {
         }
     }
 
+    static int power(int x, int y) {
+        // Initialize result
+        int res = 1;
+
+        while (y > 0) {
+            // If y is odd,
+            // multiply
+            // x with result
+            if ((y & 1) == 1)
+                res = res * x;
+
+            // y must be even now
+            y = y >> 1; // y = y/2
+            x = x * x; // Change x to x^2
+        }
+        return res;
+    }
+    
+    static int leftShift(int n , int totalbitshift){
+        return n*power(2,totalbitshift);
+    }
+
+    static int rightShift(int n, int totalbitshift) {
+        return n / power(2, totalbitshift);
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> result = binary(12, new ArrayList<>());
         Collections.reverse(result);
         System.out.println(result);
+        System.out.println(leftShift(8, 2));
+        System.out.println(rightShift(16, 4));
     }
 }
